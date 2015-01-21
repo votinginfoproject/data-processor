519d79d3e81da786a428fb65dc41193b945ac65b
Chris Shea <cmshea@gmail.com>
2015-01-21 16:06:50 -0500

    [CS NH] Log instead of printing to the screen

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject vip.data-processor "0.1.0-SNAPSHOT"
   :description "Voting Information Project Data Processor"
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [joda-time "2.7"]
                  [clj-aws-s3 "0.3.10" :exclusions [joda-time]]
                  [democracyworks.squishy "1.0.0" :exclusions [joda-time]]
--- src/vip/data_processor.clj
+++ src/vip/data_processor.clj
@@ -1,5 +1,6 @@
 (ns vip.data-processor
   (:require [clojure.edn :as edn]
+            [clojure.tools.logging :as log]
             [democracyworks.squishy :as sqs]
             [vip.data-processor.s3 :as s3])
   (:gen-class))
@@ -7,16 +8,16 @@
 (defn process-file [message]
   (let [{:keys [filename]} (edn/read-string (:body message))
         file (s3/download filename)]
-    (println (pr-str file))))
+    (log/info "Downloaded" (pr-str file))))
 
 (defn consume []
   (sqs/consume-messages (sqs/client) process-file))
 
 (defn -main [& args]
-  (println "VIP Data Processor starting up.")
+  (log/info "VIP Data Processor starting up.")
   (let [consumer (consume)]
     (.addShutdownHook (Runtime/getRuntime)
                       (Thread. (fn []
-                                 (println "VIP Data Processor shutting down...")
+                                 (log/info "VIP Data Processor shutting down...")
                                  (future-cancel consumer))))
     (while true)))
