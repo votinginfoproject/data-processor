f871b4f68818638d755ad738ea5b90f6bd94e622
Chris Shea <cmshea@gmail.com>
2015-01-22 11:55:25 -0500

    [CS NH] Configure logging a bit
    * Turn off logging during tests
    * Simple logging to stdout for dev and prod

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN lein deps
 
 COPY . /usr/src/data-processor
 
+RUN lein test
 RUN mv "$(lein uberjar | sed -n 's/^Created \(.*standalone\.jar\)/\1/p')" data-processor-standalone.jar
 
 CMD ["java", "-jar", "data-processor-standalone.jar"]
--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject vip.data-processor "0.1.0-SNAPSHOT"
   :description "Voting Information Project Data Processor"
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [org.slf4j/slf4j-log4j12 "1.7.10"]
                  [org.clojure/tools.logging "0.3.1"]
                  [joda-time "2.7"]
                  [clj-aws-s3 "0.3.10" :exclusions [joda-time]]
-                 [democracyworks.squishy "1.0.0" :exclusions [joda-time]]
+                 [democracyworks.squishy "1.0.0" :exclusions [joda-time
+                                                              org.slf4j/slf4j-simple]]
                  [turbovote.resource-config "0.1.3"]]
+  :profiles {:test {:resource-paths ["test-resources"]}}
   :main vip.data-processor)
