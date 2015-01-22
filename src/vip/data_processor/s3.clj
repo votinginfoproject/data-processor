95846ce33c68ff394032fb711b881b192a18da2f
Chris Shea <cmshea@gmail.com>
2015-01-22 15:21:43 -0500

    [CS NH] Correct s3/download
    Using java.nio.files.Files/copy is more straightforward and
    byte-for-byte correct.

--- src/vip/data_processor/s3.clj
+++ src/vip/data_processor/s3.clj
@@ -2,22 +2,24 @@
   (:require [aws.sdk.s3 :as s3]
             [clojure.java.io :as io]
             [turbovote.resource-config :refer [config]])
-  (:import [java.io File]))
+  (:import [java.nio.file Files CopyOption StandardCopyOption]
+           [java.nio.file.attribute FileAttribute]))
 
 (defn- get-object [key]
   (s3/get-object (config :aws :creds)
                  (config :aws :s3 :bucket)
                  key))
 
-(def tmp-file-prefix "vip-data-processor")
+(def tmp-path-prefix "vip-data-processor")
 
 (defn download
   "Downloads the file named `key` from the configured S3 bucket to a
-  temporary file and returns that file."
+  temporary file and returns that path."
   [key]
-  (let [tmp-file (File/createTempFile tmp-file-prefix key)
+  (let [tmp-path (Files/createTempFile tmp-path-prefix key
+                                       (into-array FileAttribute []))
         s3-object (get-object key)]
-    (with-open [w (io/writer tmp-file)
-                r (io/reader (:content s3-object))]
-      (doall (map #(.write w (str % "\n")) (line-seq r))))
-    tmp-file))
+    (Files/copy (:content s3-object)
+                tmp-path
+                (into-array [StandardCopyOption/REPLACE_EXISTING]))
+    tmp-path))
