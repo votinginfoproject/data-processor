c14d918ce9ac7166a952a36798f7f740edae6be2
Chris Shea <cmshea@gmail.com>
2015-01-22 15:23:08 -0500

    [CS NH] Expect zip files and unzip them

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [clj-aws-s3 "0.3.10" :exclusions [joda-time]]
                  [democracyworks.squishy "1.0.0" :exclusions [joda-time
                                                               org.slf4j/slf4j-simple]]
+                 [net.lingala.zip4j/zip4j "1.3.2"]
                  [turbovote.resource-config "0.1.3"]]
   :profiles {:test {:resource-paths ["test-resources"]}}
   :main vip.data-processor)
--- src/vip/data_processor.clj
+++ src/vip/data_processor.clj
@@ -2,13 +2,15 @@
   (:require [clojure.tools.logging :as log]
             [democracyworks.squishy :as sqs]
             [vip.data-processor.pipeline :as pipeline]
-            [vip.data-processor.validation.transforms :as t])
+            [vip.data-processor.validation.transforms :as t]
+            [vip.data-processor.validation.zip :as zip])
   (:gen-class))
 
 (def pipeline
   [t/read-edn-sqs-message
    t/assert-filename
-   t/download-from-s3])
+   t/download-from-s3
+   zip/unzip])
 
 (defn consume []
   (sqs/consume-messages (sqs/client) (partial pipeline/process pipeline)))
