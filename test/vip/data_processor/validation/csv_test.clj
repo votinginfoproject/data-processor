c32107b2819d5a768bdf169cf5ca51b7919ee190
B. Wesley Reid <bwreid@gmail.com>
2015-02-19 09:19:42 -0700

    [CS WR] add tests for load-elections

--- test/vip/data_processor/validation/csv_test.clj
+++ test/vip/data_processor/validation/csv_test.clj
@@ -1,6 +1,9 @@
 (ns vip.data-processor.validation.csv-test
   (:require [clojure.test :refer :all]
-            [vip.data-processor.validation.csv :refer :all])
+            [vip.data-processor.validation.csv :refer :all]
+            [vip.data-processor.db.sqlite :as sqlite]
+            [clojure.java.io :as io]
+            [korma.core :as korma])
   (:import [java.io File]))
 
 (deftest remove-bad-filenames-test
@@ -16,3 +19,24 @@
         (is (get-in out-ctx [:warnings :validate-filenames]))
         (is (not-every? good-filename? (:input ctx)))
         (is (every? good-filename? (:input out-ctx)))))))
+
+(deftest load-elections-test
+  (testing "with a valid election.txt file"
+    (let [db (sqlite/temp-db "load-elections-test")
+          ctx (merge {:input [(io/as-file (io/resource "election.txt"))]}
+                     db)]
+      (is (empty? (korma/select (get-in ctx [:tables :elections]))))
+      (testing "inserts rows from the valid election.txt file"
+        (let [out-ctx (load-elections ctx)]
+          (is (= '({:id 5400} {:id 5401})
+                 (korma/select (get-in out-ctx [:tables :elections])
+                               (korma/fields :id))))
+          (testing "no/yes values are turned into 0/1"
+            (is (= '({:id 5400 :statewide 1} {:id 5401 :statewide 0})
+                   (korma/select (get-in out-ctx [:tables :elections])
+                                 (korma/fields :id :statewide)))))))))
+  (testing "with no election.txt file the ctx remains the same"
+    (let [db (sqlite/temp-db "no-load-elections-test")
+          ctx (merge {:input []} db)
+          out-ctx (load-elections (assoc ctx :input []))]
+      (is (empty? (korma/select (get-in out-ctx [:tables :elections])))))))
