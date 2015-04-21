94760f0ef444798b4244619a10e164817b9069c4
Chris Shea <cmshea@gmail.com>
2015-04-21 17:16:59 -0400

    [CS] Output election to XML

--- src/vip/data_processor/output/xml.clj
+++ src/vip/data_processor/output/xml.clj
@@ -22,6 +22,7 @@
             [vip.data-processor.output.candidate :as candidate]
             [vip.data-processor.output.contest :as contest]
             [vip.data-processor.output.early-vote-site :as early-vote-site]
+            [vip.data-processor.output.election :as election]
             [vip.data-processor.output.election-official :as election-official]
             [vip.data-processor.output.polling-location :as polling-location]
             [vip.data-processor.output.referendum :as referendum]
@@ -94,6 +95,7 @@
    (add-xml-children candidate/xml-nodes)
    (add-xml-children contest/xml-nodes)
    (add-xml-children early-vote-site/xml-nodes)
+   (add-xml-children election/xml-nodes)
    (add-xml-children election-official/xml-nodes)
    (add-xml-children polling-location/xml-nodes)
    (add-xml-children referendum/xml-nodes)
