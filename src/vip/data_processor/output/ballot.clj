6e331ceb65c25476d1ab3d2b97134a32e82e148b
Chris Shea <cmshea@gmail.com>
2015-04-23 13:23:45 -0400

    [CS] Move and docstring `output.ballot/candidates`

--- src/vip/data_processor/output/ballot.clj
+++ src/vip/data_processor/output/ballot.clj
@@ -2,6 +2,19 @@
   (:require [vip.data-processor.output.xml-helpers :refer :all]
             [korma.core :as korma]))
 
+(defn candidates
+  "Find candidates belonging to a ballot. Goes through the
+  ballot_candidates table to grab their `id` and `sort_order` columns,
+  which is all that's needed for the ballot's XML representation."
+  [ctx ballot]
+  (let [ballot-candidates-table (get-in ctx [:tables :ballot-candidates])
+        candidates-table (get-in ctx [:tables :candidates])]
+    (korma/select ballot-candidates-table
+                  (korma/fields :candidates.id :candidates.sort_order)
+                  (korma/join candidates-table
+                              (= :candidates.id :candidate_id))
+                  (korma/where {:ballot_id (:id ballot)}))))
+
 (defn candidate->xml [candidate]
   (let [xml {:tag :candidate_id
              :content [(str (:id candidate))]}
@@ -22,15 +35,6 @@
                           (xml-node image_url)]
                          (map candidate->xml candidates))]
     (simple-xml :ballot id children)))
-
-(defn candidates [ctx ballot]
-  (let [ballot-candidates-table (get-in ctx [:tables :ballot-candidates])
-        candidates-table (get-in ctx [:tables :candidates])]
-    (korma/select ballot-candidates-table
-                  (korma/fields :candidates.id :candidates.sort_order)
-                  (korma/join candidates-table
-                              (= :candidates.id :candidate_id))
-                  (korma/where {:ballot_id (:id ballot)}))))
 
 (defn xml-nodes [ctx]
   (let [ballots-table (get-in ctx [:tables :ballots])
