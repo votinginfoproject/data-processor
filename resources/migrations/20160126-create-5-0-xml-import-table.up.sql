2b023a68596186193e38995c19c1a5282ac97ac6
Chris Shea <cmshea@gmail.com>
2016-02-02 13:40:40 -0500

    [CS] Remove unneeded comment

--- resources/migrations/20160126-create-5-0-xml-import-table.up.sql
+++ resources/migrations/20160126-create-5-0-xml-import-table.up.sql
@@ -4,5 +4,3 @@ CREATE TABLE v5_0_xml_values (results_id BIGINT REFERENCES results (id) NOT NULL
                               path ltree NOT NULL,
                               value TEXT,
                               parent_with_id ltree);
-
--- 'VipObject.0.BallotStyle.1717.ImageUri.0', 'http://...', 'VipObject.0.BallotStyle.1717'
