c6e04338ac885a404571ae34ae89710617cd675e
Chris Shea <cmshea@gmail.com>
2016-02-08 11:31:26 -0500

    [CS] Add indexes to xml_tree_values

--- resources/migrations/20160126-create-xml-tree-values-table.down.sql
+++ resources/migrations/20160126-create-xml-tree-values-table.down.sql
@@ -1,1 +1,4 @@
+DROP INDEX xml_tree_values_result_path_idx;
+DROP INDEX xml_tree_values_gist_path_idx;
+
 DROP TABLE xml_tree_values;
--- resources/migrations/20160126-create-xml-tree-values-table.up.sql
+++ resources/migrations/20160126-create-xml-tree-values-table.up.sql
@@ -4,3 +4,6 @@ CREATE TABLE xml_tree_values (results_id BIGINT REFERENCES results (id) NOT NULL
                               path ltree NOT NULL,
                               value TEXT,
                               parent_with_id ltree);
+
+CREATE INDEX xml_tree_values_result_path_idx ON xml_tree_values (results_id, path);
+CREATE INDEX xml_tree_values_gist_path_idx ON xml_tree_values USING GIST (path);
