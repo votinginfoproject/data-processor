ef7aa7fc9644d7cd010f4f5d93f63980db4538c7
Chris Shea <cmshea@gmail.com>
2016-02-08 14:56:42 -0500

    [CS] No GIST... it doesn't help us!

--- resources/migrations/20160126-create-xml-tree-values-table.down.sql
+++ resources/migrations/20160126-create-xml-tree-values-table.down.sql
@@ -1,4 +1,3 @@
 DROP INDEX xml_tree_values_result_path_idx;
-DROP INDEX xml_tree_values_gist_path_idx;
 
 DROP TABLE xml_tree_values;
--- resources/migrations/20160126-create-xml-tree-values-table.up.sql
+++ resources/migrations/20160126-create-xml-tree-values-table.up.sql
@@ -6,4 +6,3 @@ CREATE TABLE xml_tree_values (results_id BIGINT REFERENCES results (id) NOT NULL
                               parent_with_id ltree);
 
 CREATE INDEX xml_tree_values_result_path_idx ON xml_tree_values (results_id, path);
-CREATE INDEX xml_tree_values_gist_path_idx ON xml_tree_values USING GIST (path);
