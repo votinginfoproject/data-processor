70ee2c29f881049ec4d01ee4d6d40f9432129cb8
Wes Morgan <cap10morgan@gmail.com>
2016-03-02 15:32:41 -0700

    Validate Candidate PreElectionStatus

--- src/vip/data_processor/validation/v5/candidate.clj
+++ src/vip/data_processor/validation/v5/candidate.clj
@@ -3,9 +3,26 @@
             [vip.data-processor.db.postgres :as postgres]
             [vip.data-processor.validation.data-spec.value-format :as value-format]))
 
+(defn valid-pre-election-status? [status]
+  (#{"filed" "qualified" "withdrawn" "write-in"} (:value status)))
+
 (defn valid-post-election-status? [status]
   (#{"advanced-to-runoff" "projected-winner" "winner" "withdrawn"}
    (:value status)))
+
+(defn validate-pre-election-statuses [{:keys [import-id] :as ctx}]
+  (let [statuses (korma/select postgres/xml-tree-values
+                               (korma/where {:results_id import-id})
+                               (korma/where
+                                (postgres/ltree-match
+                                 postgres/xml-tree-values :path
+                                 "VipObject.0.Candidate.*{1}.PreElectionStatus.*{1}")))
+        invalid-statuses (remove valid-pre-election-status? statuses)]
+    (reduce (fn [ctx row]
+              (update-in ctx
+                         [:errors :candidates (-> row :path .getValue) :format]
+                         conj (:value row)))
+            ctx invalid-statuses)))
 
 (defn validate-post-election-statuses [{:keys [import-id] :as ctx}]
   (let [statuses (korma/select postgres/xml-tree-values
--- test/vip/data_processor/validation/v5/candidate_test.clj
+++ test/vip/data_processor/validation/v5/candidate_test.clj
@@ -9,32 +9,62 @@
 (use-fixtures :once setup-postgres)
 (use-fixtures :each with-clean-postgres)
 
+(deftest ^:postgres validate-pre-election-statuses-test
+  (let [ctx {:input (xml-input "v5-election-statuses.xml")
+             :pipeline [psql/start-run
+                        xml/load-xml-ltree
+                        v5.candidate/validate-pre-election-statuses]}
+        out-ctx (pipeline/run-pipeline ctx)]
+    (is (not (get-in out-ctx [:errors
+                              :candidates
+                              "VipObject.0.Candidate.0.PreElectionStatus.0"])))
+    (is (not (get-in out-ctx [:errors
+                              :candidates
+                              "VipObject.0.Candidate.1.PreElectionStatus.0"])))
+    (is (not (get-in out-ctx [:errors
+                              :candidates
+                              "VipObject.0.Candidate.2.PreElectionStatus.0"])))
+    (is (not (get-in out-ctx [:errors
+                              :candidates
+                              "VipObject.0.Candidate.3.PreElectionStatus.0"])))
+    (is (get-in out-ctx [:errors
+                         :candidates
+                         "VipObject.0.Candidate.4.PreElectionStatus.0"
+                         :format]))
+    (is (get-in out-ctx [:errors
+                         :candidates
+                         "VipObject.0.Candidate.5.PreElectionStatus.0"
+                         :format]))
+    (is (not (get-in out-ctx [:errors
+                              :candidates
+                              "VipObject.0.Candidate.6.PreElectionStatus.0"])))))
+
 (deftest ^:postgres validate-post-election-statuses-test
-  (let [ctx {:input (xml-input "v5-post-election-statuses.xml")
+  (let [ctx {:input (xml-input "v5-election-statuses.xml")
              :pipeline [psql/start-run
                         xml/load-xml-ltree
                         v5.candidate/validate-post-election-statuses]}
         out-ctx (pipeline/run-pipeline ctx)]
     (is (not (get-in out-ctx [:errors
                               :candidates
-                              "VipObject.0.Candidate.0.PostElectionStatus.0"])))
+                              "VipObject.0.Candidate.0.PostElectionStatus.1"])))
     (is (not (get-in out-ctx [:errors
                               :candidates
-                              "VipObject.0.Candidate.1.PostElectionStatus.0"])))
+                              "VipObject.0.Candidate.1.PostElectionStatus.1"])))
     (is (not (get-in out-ctx [:errors
                               :candidates
-                              "VipObject.0.Candidate.2.PostElectionStatus.0"])))
+                              "VipObject.0.Candidate.2.PostElectionStatus.1"])))
     (is (not (get-in out-ctx [:errors
                               :candidates
-                              "VipObject.0.Candidate.3.PostElectionStatus.0"])))
+                              "VipObject.0.Candidate.3.PostElectionStatus.1"])))
     (is (get-in out-ctx [:errors
                          :candidates
-                         "VipObject.0.Candidate.4.PostElectionStatus.0"
+                         "VipObject.0.Candidate.4.PostElectionStatus.1"
                          :format]))
     (is (get-in out-ctx [:errors
                          :candidates
-                         "VipObject.0.Candidate.5.PostElectionStatus.0"
+                         "VipObject.0.Candidate.5.PostElectionStatus.1"
                          :format]))
     (is (not (get-in out-ctx [:errors
                               :candidates
-                              "VipObject.0.Candidate.6.PostElectionStatus.0"])))))
+                              "VipObject.0.Candidate.6.PostElectionStatus.1"])))))
