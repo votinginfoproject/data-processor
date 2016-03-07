556ae735f6416b3d61b3fad09028f39dacf9dc67
Chris Shea <cmshea@gmail.com>
2016-03-07 15:52:00 -0500

    Use util/build-xml-tree-value-query-validator more
    Candidate and Id validations updated to use the
    build-xml-tree-value-query-validator helper.

--- src/vip/data_processor/validation/v5/candidate.clj
+++ src/vip/data_processor/validation/v5/candidate.clj
@@ -1,7 +1,7 @@
 (ns vip.data-processor.validation.v5.candidate
   (:require [korma.core :as korma]
             [vip.data-processor.db.postgres :as postgres]
-            [vip.data-processor.validation.data-spec.value-format :as value-format]))
+            [vip.data-processor.validation.v5.util :as util]))
 
 (defn valid-pre-election-status? [status]
   (#{"filed" "qualified" "withdrawn" "write-in"} (:value status)))
@@ -38,32 +38,14 @@
                          conj (:value row)))
             ctx invalid-statuses)))
 
-(def missing-ballot-name-query-string
-  "SELECT xtv.path
+(def validate-no-missing-ballot-names
+  (util/build-xml-tree-value-query-validator
+   :errors :candidates :missing :missing-ballot-name
+   "SELECT xtv.path
     FROM (SELECT DISTINCT subltree(path, 0, 4) || 'BallotName' AS path
           FROM xml_tree_values WHERE results_id = ?
           AND subltree(path, 0, 4) ~ 'VipObject.0.Candidate.*{1}') xtv
     LEFT JOIN (SELECT path FROM xml_tree_values WHERE results_id = ?) xtv2
     ON xtv.path = subltree(xtv2.path, 0, 5)
-    WHERE xtv2.path IS NULL")
-
-(defn missing-ballot-name-query
-  "Generates a query vector for korma's exec-raw to find missing BallotNames."
-  [import-id]
-  [missing-ballot-name-query-string [import-id import-id]])
-
-(defn missing-ballot-names
-  "For an import, find all the BallotName paths that should exist but don't."
-  [import-id]
-  (korma/exec-raw
-   (:conn postgres/xml-tree-values)
-   (missing-ballot-name-query import-id)
-   :results))
-
-(defn validate-no-missing-ballot-names [{:keys [import-id] :as ctx}]
-  (->> (missing-ballot-names import-id)
-       (map :path)
-       (reduce (fn [ctx path]
-                 (update-in ctx [:errors :candidates (.getValue path) :missing]
-                            conj :missing-ballot-name))
-               ctx)))
+    WHERE xtv2.path IS NULL"
+   util/two-import-ids))
--- src/vip/data_processor/validation/v5/id.clj
+++ src/vip/data_processor/validation/v5/id.clj
@@ -1,6 +1,7 @@
 (ns vip.data-processor.validation.v5.id
   (:require [korma.core :as korma]
-            [vip.data-processor.db.postgres :as postgres]))
+            [vip.data-processor.db.postgres :as postgres]
+            [vip.data-processor.validation.v5.util :as util]))
 
 (defn duplicate-ids [import-id]
   (korma/select [postgres/xml-tree-values :first]
@@ -22,39 +23,14 @@
                 (update-in ctx [:fatal :id path :duplicates] conj id)))
             ctx duplicate-ids)))
 
-(def missing-id-query-string
-  "A close reading of the 5.0 and 5.1 spec reveals that only direct
-  children of the VipObject element require an id attribute and that
-  *every* direct child of the VipObject requires an id
-  attribute. Thus, this query generates every id path that should
-  exist (in the first subquery), and then left joins against paths
-  that do exist to find the ones that are missing."
-  "SELECT xtv.path
+(def validate-no-missing-ids
+  (util/build-xml-tree-value-query-validator
+   :fatal :id :missing :missing-id
+   "SELECT xtv.path
     FROM (SELECT DISTINCT subltree(path, 0, 4) || 'id' AS path
           FROM xml_tree_values WHERE results_id = ?
           AND nlevel(subltree(path, 0, 4)) = 4) xtv
     LEFT JOIN (SELECT path FROM xml_tree_values WHERE results_id = ?) xtv2
     ON xtv.path = xtv2.path
-    WHERE xtv2.path IS NULL")
-
-(defn missing-id-query
-  "Generates a query vector for korma's exec-raw to find missing ids."
-  [import-id]
-  [missing-id-query-string [import-id import-id]])
-
-(defn missing-ids
-  "For an import, find all the id paths that should exist but don't."
-  [import-id]
-  (korma/exec-raw
-   (:conn postgres/xml-tree-values)
-   (missing-id-query import-id)
-   :results))
-
-(defn validate-no-missing-ids
-  [{:keys [import-id] :as ctx}]
-  (->> (missing-ids import-id)
-       (map :path)
-       (reduce (fn [ctx path]
-                 (update-in ctx [:fatal :id (.getValue path) :missing]
-                            conj :missing-id))
-               ctx)))
+    WHERE xtv2.path IS NULL"
+   util/two-import-ids))
