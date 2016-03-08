6080122ca35284f7b37fa5030a31516ca53dad76
Wes Morgan <cap10morgan@gmail.com>
2016-03-08 12:45:55 -0700

    Add Source validations to v5 validations

--- src/vip/data_processor/validation/v5.clj
+++ src/vip/data_processor/validation/v5.clj
@@ -2,7 +2,8 @@
   (:require [vip.data-processor.validation.v5.candidate :as candidate]
             [vip.data-processor.validation.v5.email :as email]
             [vip.data-processor.validation.v5.id :as id]
-            [vip.data-processor.validation.v5.precinct :as precinct]))
+            [vip.data-processor.validation.v5.precinct :as precinct]
+            [vip.data-processor.validation.v5.source :as source]))
 
 (def validations
   [candidate/validate-no-missing-ballot-names
@@ -12,4 +13,9 @@
    id/validate-unique-ids
    id/validate-no-missing-ids
    precinct/validate-no-missing-names
-   precinct/validate-no-missing-locality-ids])
+   precinct/validate-no-missing-locality-ids
+   source/validate-one-source
+   source/validate-name
+   source/validate-date-time
+   source/validate-vip-id
+   source/validate-vip-id-valid-fips])
