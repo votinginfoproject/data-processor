0e159848e2b93711b011469d4b2ecf4728969712
Chris Shea <cmshea@gmail.com>
2016-03-21 10:24:16 -0400

    Validate IDREF's refer to something

--- src/vip/data_processor/validation/v5/id.clj
+++ src/vip/data_processor/validation/v5/id.clj
@@ -1,7 +1,8 @@
 (ns vip.data-processor.validation.v5.id
   (:require [korma.core :as korma]
             [vip.data-processor.db.postgres :as postgres]
-            [vip.data-processor.validation.v5.util :as util]))
+            [vip.data-processor.validation.v5.util :as util]
+            [vip.data-processor.validation.xml.spec :as spec]))
 
 (defn duplicate-ids [import-id]
   (korma/select [postgres/xml-tree-values :first]
@@ -34,3 +35,28 @@
     ON xtv.path = xtv2.path
     WHERE xtv2.path IS NULL"
    util/two-import-ids))
+
+(defn validate-idref-type-refers
+  [{:keys [import-id] :as ctx} simple-path]
+  (let [bad-ids (korma/exec-raw
+                 (:conn postgres/xml-tree-values)
+                 ["SELECT xtv.path, xtv.value FROM xml_tree_values xtv
+                   LEFT JOIN (SELECT value FROM xml_tree_values
+                              WHERE results_id = ? AND simple_path ~ '*{2}.id') xtv2
+                          ON xtv.value = xtv2.value
+                   WHERE xtv.results_id = ?
+                   AND xtv.simple_path = ?
+                   AND xtv2.value IS NULL"
+                  [import-id import-id simple-path]]
+                 :results)]
+    (reduce (fn [ctx bad-id]
+              (update-in ctx [:errors :id (.getValue (:path bad-id)) :no-referent]
+                         conj (str (:value bad-id))))
+            ctx bad-ids)))
+
+(defn validate-idrefs-refer
+  [{:keys [import-id spec-version] :as ctx}]
+  (let [idref-simple-paths (->> (spec/type->simple-paths "xs:IDREF" spec-version)
+                                (map postgres/path->ltree))]
+    (reduce validate-idref-type-refers
+            ctx idref-simple-paths)))
