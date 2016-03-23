95591fd0b3d52bd67c73ae2ae2e1b2295a2b3b4c
Wes Morgan <cap10morgan@gmail.com>
2016-03-23 16:09:36 -0600

    Adapt Locality validations to util no-missing
    I centralized & generalized this for InternationalizedText, so this commit adapts the original validations to use the first version of the build-no-missing-validators fn.

--- src/vip/data_processor/validation/v5/locality.clj
+++ src/vip/data_processor/validation/v5/locality.clj
@@ -1,9 +1,8 @@
 (ns vip.data-processor.validation.v5.locality
   (:require [vip.data-processor.validation.v5.util :as util]
-            [vip.data-processor.validation.xml.spec :as spec]
-            [clojure.string :as str]
             [korma.core :as korma]
-            [vip.data-processor.db.postgres :as postgres]))
+            [vip.data-processor.db.postgres :as postgres]
+            [vip.data-processor.validation.xml.spec :as spec]))
 
 (def valid-types
   #{"city" "city-council" "congressional" "county" "county-concil" "judicial"
@@ -11,39 +10,19 @@
     "state-senate" "town" "township" "utility" "village" "ward" "water"
     "other"})
 
-(def locality-paths
-  (spec/type->simple-paths "Locality" "5.0"))
-
-(defn clojure-type->xml-type [type]
-  (let [components (-> type name (str/split #"-"))]
-    (str/join (map str/capitalize components))))
-
-(defn build-no-missing-validators [type import-id]
-  (let [path-element (clojure-type->xml-type type)]
-    (for [p locality-paths]
-      (util/build-xml-tree-value-query-validator
-       :errors :locality :missing (->> type name (str "missing-") keyword)
-       "SELECT xtv.path
-        FROM (SELECT DISTINCT subltree(path, 0, 4) || ? AS path
-              FROM xml_tree_values WHERE results_id = ?
-              AND subltree(simple_path, 0, 2) = text2ltree(?)) xtv
-        LEFT JOIN (SELECT path FROM xml_tree_values WHERE results_id = ?) xtv2
-        ON xtv.path = subltree(xtv2.path, 0, 5)
-        WHERE xtv2.path IS NULL"
-       (constantly [path-element import-id p import-id])))))
-
 (defn validate-no-missing-names [{:keys [import-id] :as ctx}]
-  (let [validators (build-no-missing-validators :name import-id)]
+  (let [validators (util/build-no-missing-validators :locality :name import-id)]
     (reduce (fn [ctx validator] (validator ctx)) ctx validators)))
 
 (defn validate-no-missing-state-ids [{:keys [import-id] :as ctx}]
-  (let [validators (build-no-missing-validators :state-id import-id)]
+  (let [validators (util/build-no-missing-validators :locality :state-id
+                                                     import-id)]
     (reduce (fn [ctx validator] (validator ctx)) ctx validators)))
 
 (defn valid-type? [type] (valid-types type))
 
 (defn validate-types [ctx]
-  (let [validators (for [p locality-paths]
+  (let [validators (for [p (spec/type->simple-paths "Locality" "5.0")]
                      (fn [{:keys [import-id] :as ctx}]
                        (let [type-path (str p ".Type")
                              types (korma/select postgres/xml-tree-values
