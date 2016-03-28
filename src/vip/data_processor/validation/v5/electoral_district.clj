2cf7745c51ad5c94b0cb262935dfe6895c65945f
Wes Morgan <cap10morgan@gmail.com>
2016-03-28 15:46:22 -0600

    Use validate-no-missing-elements for name & type

--- src/vip/data_processor/validation/v5/electoral_district.clj
+++ src/vip/data_processor/validation/v5/electoral_district.clj
@@ -2,28 +2,10 @@
   (:require [vip.data-processor.validation.v5.util :as util]))
 
 (def validate-no-missing-names
-  (util/build-xml-tree-value-query-validator
-   :errors :electoral-district :missing :missing-name
-   "SELECT xtv.path
-    FROM (SELECT DISTINCT subltree(path, 0, 4) || 'Name' AS path
-          FROM xml_tree_values WHERE results_id = ?
-          AND subltree(path, 0, 4) ~ 'VipObject.0.ElectoralDistrict.*{1}') xtv
-    LEFT JOIN (SELECT path FROM xml_tree_values WHERE results_id = ?) xtv2
-    ON xtv.path = subltree(xtv2.path, 0, 5)
-    WHERE xtv2.path IS NULL"
-   util/two-import-ids))
+  (util/validate-no-missing-elements :electoral-district :name))
 
 (def validate-no-missing-types
-  (util/build-xml-tree-value-query-validator
-   :errors :electoral-district :missing :missing-type
-   "SELECT xtv.path
-    FROM (SELECT DISTINCT subltree(path, 0, 4) || 'Type' AS path
-          FROM xml_tree_values WHERE results_id = ?
-          AND subltree(path, 0, 4) ~ 'VipObject.0.ElectoralDistrict.*{1}') xtv
-    LEFT JOIN (SELECT path FROM xml_tree_values WHERE results_id = ?) xtv2
-    ON xtv.path = subltree(xtv2.path, 0, 5)
-    WHERE xtv2.path IS NULL"
-   util/two-import-ids))
+  (util/validate-no-missing-elements :electoral-district :type))
 
 (def types
   #{"city" "city-council" "congressional" "county" "county-council" "judicial"
