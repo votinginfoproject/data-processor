3bcdff555af1b4a3816ee39f3005f90bdc97c71e
Wes Morgan <cap10morgan@gmail.com>
2016-03-29 11:54:46 -0600

    Add OrderedContest v5 validations

---
+++ src/vip/data_processor/validation/v5/ordered_contest.clj
@@ -0,0 +1,5 @@
+(ns vip.data-processor.validation.v5.ordered-contest
+  (:require [vip.data-processor.validation.v5.util :as util]))
+
+(def validate-no-missing-contest-ids
+  (util/validate-no-missing-elements :ordered-contest :contest-id))
