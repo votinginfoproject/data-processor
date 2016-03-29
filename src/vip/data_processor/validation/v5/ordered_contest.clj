1c97db46cd1b744779a3b0fe631df3250ed622ad
Wes Morgan <cap10morgan@gmail.com>
2016-03-29 14:22:23 -0600

    Fix OrderedContest validation
    util/validate-no-missing-elements now takes a vector element-path.

--- src/vip/data_processor/validation/v5/ordered_contest.clj
+++ src/vip/data_processor/validation/v5/ordered_contest.clj
@@ -2,4 +2,4 @@
   (:require [vip.data-processor.validation.v5.util :as util]))
 
 (def validate-no-missing-contest-ids
-  (util/validate-no-missing-elements :ordered-contest :contest-id))
+  (util/validate-no-missing-elements :ordered-contest [:contest-id]))
