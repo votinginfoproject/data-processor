2abfc96677ca484d71693ea44aed2f9d7421d521
Wes Morgan <cap10morgan@gmail.com>
2016-03-29 11:32:07 -0600

    Add Office validations to v5 validations

--- src/vip/data_processor/validation/v5.clj
+++ src/vip/data_processor/validation/v5.clj
@@ -11,7 +11,8 @@
             [vip.data-processor.validation.v5.election-administration :as election-admin]
             [vip.data-processor.validation.v5.election :as election]
             [vip.data-processor.validation.v5.locality :as locality]
-            [vip.data-processor.validation.v5.internationalized-text :as intl-text]))
+            [vip.data-processor.validation.v5.internationalized-text :as intl-text]
+            [vip.data-processor.validation.v5.office :as office]))
 
 (def validations
   [candidate/validate-no-missing-ballot-names
@@ -41,4 +42,7 @@
    locality/validate-no-missing-names
    locality/validate-no-missing-state-ids
    locality/validate-types
-   intl-text/validate-no-missing-texts])
+   intl-text/validate-no-missing-texts
+   office/validate-no-missing-names
+   office/validate-no-missing-term-types
+   office/validate-term-types])
