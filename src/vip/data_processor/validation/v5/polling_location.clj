7e817c982a2523de9163b941c4521a77046e5f37
Wes Morgan <cap10morgan@gmail.com>
2016-04-04 16:18:01 -0600

    Validate PollingLocations

--- src/vip/data_processor/validation/v5/polling_location.clj
+++ src/vip/data_processor/validation/v5/polling_location.clj
@@ -1,5 +1,24 @@
 (ns vip.data-processor.validation.v5.polling-location
-  (:require [vip.data-processor.validation.v5.util :as util]))
+  (:require [vip.data-processor.validation.v5.util :as util]
+            [clojure.edn :as edn]))
 
 (def validate-no-missing-address-lines
   (util/validate-no-missing-elements :polling-location [:address-line]))
+
+(def validate-no-missing-latitudes
+  (util/validate-no-missing-elements :lat-lng [:latitude]))
+
+(def validate-no-missing-longitudes
+  (util/validate-no-missing-elements :lat-lng [:longitude]))
+
+(def validate-latitude
+  (util/validate-elements :lat-lng
+                          [:latitude]
+                          (comp float? edn/read-string)
+                          :errors :format))
+
+(def validate-longitude
+  (util/validate-elements :lat-lng
+                          [:longitude]
+                          (comp float? edn/read-string)
+                          :errors :format))
--- test/vip/data_processor/validation/v5/polling_location_test.clj
+++ test/vip/data_processor/validation/v5/polling_location_test.clj
@@ -22,3 +22,42 @@
       (is (not (get-in out-ctx [:errors :polling-location
                                 "VipObject.0.PollingLocation.1.AddressLine"
                                 :missing]))))))
+
+(deftest ^:postgres validate-latitude-longitude-test
+  (let [ctx {:input (xml-input "v5-polling-locations.xml")}
+        out-ctx (-> ctx
+                    psql/start-run
+                    xml/load-xml-ltree
+                    v5.polling-location/validate-no-missing-latitudes
+                    v5.polling-location/validate-no-missing-longitudes
+                    v5.polling-location/validate-latitude
+                    v5.polling-location/validate-longitude)]
+    (testing "lat-lng missing is OK"
+      (is (not (get-in out-ctx [:errors :lat-lng
+                                "VipObject.0.PollingLocation.0.LatLng"
+                                :missing])))
+      (is (not (get-in out-ctx [:errors :lat-lng
+                                "VipObject.0.PollingLocation.1.LatLng"
+                                :missing]))))
+    (testing "valid lat-lng is OK"
+      (is (not (get-in out-ctx [:errors :lat-lng
+                                "VipObject.0.PollingLocation.2.LatLng.1.Latitude.0"
+                                :format])))
+      (is (not (get-in out-ctx [:errors :lat-lng
+                                "VipObject.0.PollingLocation.2.LatLng.1.Longitude.1"
+                                :format]))))
+    (testing "missing latitude is an error"
+      (is (get-in out-ctx [:errors :lat-lng
+                           "VipObject.0.PollingLocation.3.LatLng.1.Latitude"
+                           :missing])))
+    (testing "missing longitude is an error"
+      (is (get-in out-ctx [:errors :lat-lng
+                           "VipObject.0.PollingLocation.4.LatLng.1.Longitude"
+                           :missing]))
+      (is (get-in out-ctx [:errors :lat-lng
+                           "VipObject.0.PollingLocation.5.LatLng.1.Longitude"
+                           :missing])))
+    (testing "invalid latitude is an error"
+      (is (get-in out-ctx [:errors :lat-lng
+                           "VipObject.0.PollingLocation.5.LatLng.1.Latitude.0"
+                           :format])))))
