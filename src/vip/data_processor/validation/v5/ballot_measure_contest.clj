ac4eb67d89172cc0c5a84c3755d643daa6b37e93
Chris Shea <cmshea@gmail.com>
2016-04-05 14:19:25 -0400

    Remove unused requires

--- src/vip/data_processor/validation/v5/ballot_measure_contest.clj
+++ src/vip/data_processor/validation/v5/ballot_measure_contest.clj
@@ -1,7 +1,5 @@
 (ns vip.data-processor.validation.v5.ballot-measure-contest
-  (:require [vip.data-processor.validation.v5.util :as util]
-            [korma.core :as korma]
-            [vip.data-processor.db.postgres :as postgres]))
+  (:require [vip.data-processor.validation.v5.util :as util]))
 
 (def validate-ballot-measure-types
   (util/validate-enum-elements :ballot-measure-type :errors))
