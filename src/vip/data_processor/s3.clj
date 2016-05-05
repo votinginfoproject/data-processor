0837d8a780c6640d8bda2de03da4272d43a9a2bb
Chris Shea <cmshea@gmail.com>
2016-05-05 11:18:44 -0400

    Create zip file in a temp directory
    Fixes the problem where a zip file can carry over between processing
    runs of feeds for the same election and causing problems when trying to
    recreate the zip file.

--- src/vip/data_processor/s3.clj
+++ src/vip/data_processor/s3.clj
@@ -53,8 +53,10 @@
   "Uploads the generated xml file to the specified S3 bucket."
   [ctx]
   (let [zip-name (str (zip-filename ctx) ".zip")
-        zip-file (File. zip-name)
-        zip (ZipFile. zip-name)
+        zip-dir (Files/createTempDirectory tmp-path-prefix
+                                           (into-array FileAttribute []))
+        zip-file (File. (.toFile zip-dir) zip-name)
+        zip (ZipFile. zip-file)
         zip-params (doto (ZipParameters.)
                      (.setCompressionLevel
                       Zip4jConstants/DEFLATE_LEVEL_NORMAL))
