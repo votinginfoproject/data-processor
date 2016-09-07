594634e993859e3af264bb45c030fc2e90cf25b7
Sean Duckett <sduckett@gmail.com>
2016-09-07 10:39:03 -0600

    Create indices on (results_id, severity)

---
+++ resources/migrations/20160907-add-indices-to-validation-tables.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX validations_results_id_severity_idx ON validations (results_id, severity);
+CREATE INDEX xml_tree_validations_results_id_severity_idx ON xml_tree_validations (results_id, severity);
