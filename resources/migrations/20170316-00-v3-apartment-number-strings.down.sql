905d06da06a427d5ed637b310458ef3d176533df
Katrina Hall <katrina.m.hall@gmail.com>
2017-03-20 12:13:19 -0400

    down migration works to handle text that cannot be cast as an integer

--- resources/migrations/20170316-00-v3-apartment-number-strings.down.sql
+++ resources/migrations/20170316-00-v3-apartment-number-strings.down.sql
@@ -1,2 +1,21 @@
-alter table v3_0_street_segments alter column start_apartment_number set data type integer using start_apartment_number::integer;
-alter table v3_0_street_segments alter column end_apartment_number set data type integer using end_apartment_number::integer;
+/*update v3_0_street_segments set start_apartment_number = null where start_apartment_number = '';*/
+/*update v3_0_street_segments set end_apartment_number = null where end_apartment_number = '';*/
+
+create or replace function convert_to_integer(apt_number_txt text)
+returns integer as $$
+declare apt_number_int integer default null;
+begin
+    begin
+        apt_number_int := apt_number_txt::integer;
+    exception when others then
+        return null;
+    end;
+return apt_number_int;
+end;
+$$ language plpgsql;
+
+
+alter table v3_0_street_segments alter column start_apartment_number set data type integer using convert_to_integer(start_apartment_number);
+alter table v3_0_street_segments alter column end_apartment_number set data type integer using convert_to_integer(end_apartment_number);
+
+drop function convert_to_integer(text);
