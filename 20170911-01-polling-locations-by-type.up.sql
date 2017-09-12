18fdec654cdb8b31b2a5aa06167689b7372f977f
Katrina Hall <katrina.m.hall@gmail.com>
2017-09-12 10:59:57 -0400

    function works to update v5_statistics table

--- 20170911-01-polling-locations-by-type.up.sql
+++ 20170911-01-polling-locations-by-type.up.sql
@@ -3,29 +3,29 @@ drop function if exists v5_dashboard.polling_locations_by_type (integer);
 create function v5_dashboard.polling_locations_by_type(rid int)
 returns table(results_id int,
               polling_location_count int,
-              polling_location_errors int,
-              polling_location_completion int,
+              pl_errors int,
+              pl_completion int,
               db_polling_location_count int,
-              db_polling_location_errors int,
-              db_polling_location_completion int,
+              db_pl_errors int,
+              db_pl_completion int,
               ev_polling_location_count int,
-              ev_polling_location_errors int,
-              ev_polling_location_completion int)
+              ev_pl_errors int,
+              ev_pl_completion int)
 as $$
   declare
     polling_location_all_paths ltree[];
     polling_location_paths ltree[];
     polling_location_count int;
-    polling_location_errors int;
-    polling_location_completion int;
+    pl_errors int;
+    pl_completion int;
     db_polling_location_paths ltree[];
     db_polling_location_count int;
-    db_polling_location_errors int;
-    db_polling_location_completion int;
+    db_pl_errors int;
+    db_pl_completion int;
     ev_polling_location_paths ltree[];
     ev_polling_location_count int;
-    ev_polling_location_errors int;
-    ev_polling_location_completion int;
+    ev_pl_errors int;
+    ev_pl_completion int;
   begin
 
   select array_agg(distinct countable_path(path)) into polling_location_all_paths
@@ -41,7 +41,7 @@ as $$
       AND values.path IS NOT NULL
       AND value = 'true';
 
-  select count(*) into db_polling_location_errors
+  select count(*) into db_pl_errors
   from xml_tree_validations xtv
   where xtv.path <@ db_polling_location_paths
   and xtv.results_id = rid;
@@ -53,7 +53,7 @@ as $$
       AND values.path IS NOT NULL
       AND value = 'true';
 
-  select count(*) into ev_polling_location_errors
+  select count(*) into ev_pl_errors
   from xml_tree_validations xtv
   where xtv.path <@ ev_polling_location_paths
   and xtv.results_id = rid;
@@ -85,18 +85,60 @@ as $$
   select pl_path from polling_location_paths except (select pl_path from db_polling_location_paths union select pl_path from ev_polling_location_paths))
   select array_agg(pl_path) from regular_polling_locations into polling_location_paths;
 
-  select count(*) into polling_location_errors
+  select count(*) into pl_errors
   from xml_tree_validations xtv
   where xtv.path <@ polling_location_paths
   and xtv.results_id = rid;
 
+  select
+    case
+      when cardinality(polling_location_paths) < pl_errors
+        then 0
+      when pl_errors = 0
+        then 100
+      else floor((cardinality(polling_location_paths) - pl_errors)/cardinality(polling_location_paths))
+    into pl_completion
+  end;
+
+  select
+    case
+      when cardinality(db_polling_location_paths) < db_pl_errors
+        then 0
+      when db_pl_errors = 0
+        then 100
+      else floor((cardinality(db_polling_location_paths) - db_pl_errors)/cardinality(db_polling_location_paths))
+    into db_pl_completion
+  end;
+
+  select
+    case
+      when cardinality(ev_polling_location_paths) < ev_pl_errors
+        then 0
+      when ev_pl_errors = 0
+        then 100
+      else floor((cardinality(ev_polling_location_paths) - ev_pl_errors)/cardinality(ev_polling_location_paths))
+    into ev_pl_completion
+  end;
+
+
+  update v5_statistics v
+    set polling_location_count = cardinality(polling_location_paths),
+        polling_location_errors = pl_errors,
+        polling_location_completion = pl_completion,
+        db_polling_location_count = cardinality(db_polling_location_paths),
+        db_polling_location_errors = db_pl_errors,
+        db_polling_location_completion = db_pl_errors,
+        ev_polling_location_count = cardinality(ev_polling_location_paths),
+        ev_polling_location_errors = ev_pl_errors,
+        ev_polling_location_completion = ev_pl_completion
+    where v.results_id = rid;
 
 
     return query
     select
-      rid, cardinality(polling_location_paths), polling_location_errors, polling_location_completion,
-      cardinality(db_polling_location_paths), db_polling_location_errors, db_polling_location_completion,
-      cardinality(ev_polling_location_paths), ev_polling_location_errors, ev_polling_location_completion;
+      rid, cardinality(polling_location_paths), pl_errors, pl_completion,
+      cardinality(db_polling_location_paths), db_pl_errors, db_pl_completion,
+      cardinality(ev_polling_location_paths), ev_pl_errors, ev_pl_completion;
 
   end;
 $$ language plpgsql;
