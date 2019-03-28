27734fc8cf71837c207954b67eda4414a47fb0a3
Troy Hill <troy@democracy.works>
2019-03-28 12:22:52 -0600

    fix tempfile prefix

--- src/vip/data_processor/s3.clj
+++ src/vip/data_processor/s3.clj
@@ -3,7 +3,7 @@
             [clojure.java.io :as io]
             [turbovote.resource-config :refer [config]]
             [korma.core :as korma]
-            [clojure.string :refer [join]]
+            [clojure.string :as str]
             [vip.data-processor.db.postgres :as postgres]
             [vip.data-processor.util :as util])
   (:import [java.io File]
@@ -31,7 +31,8 @@
   "Downloads the file named `key` from the passed in S3 `bucket` to a
   temporary file and returns that path."
   [key bucket]
-  (let [tmp-path (Files/createTempFile tmp-path-prefix key
+  (let [filename (-> key (str/split #"/") last)
+        tmp-path (Files/createTempFile tmp-path-prefix filename
                                        (into-array FileAttribute []))
         s3-object (get-object key bucket)
         stream (:input-stream s3-object)]
@@ -59,7 +60,7 @@
   (let [fips (format-fips fips)
         state (format-state state)
         date (util/format-date election-date)]
-    (str (join "-" ["vipfeed" fips state date]) ".zip")))
+    (str (str/join "-" ["vipfeed" fips state date]) ".zip")))
 
 (defn zip-filename
   [{:keys [spec-version tables import-id] :as ctx}]
