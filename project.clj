cc0148a27df0407a0096f6d545162f865e1c899b
Troy Hill <troy@democracy.works>
2019-06-11 11:10:00 -0600

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,38 +1,38 @@
 (defproject vip.data-processor "0.1.0-SNAPSHOT"
   :description "Voting Information Project Data Processor"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/tools.logging "0.5.0-alpha.1"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [com.novemberain/langohr "5.0.0"]
-                 [joda-time "2.10"]
-                 [clj-time "0.14.4"]
-                 [amazonica "0.3.132"
+                 [joda-time "2.10.2"]
+                 [clj-time "0.15.1"]
+                 [amazonica "0.3.143"
                   :exclusions [com.amazonaws/aws-java-sdk
                                com.amazonaws/amazon-kinesis-client]]
-                 [com.amazonaws/aws-java-sdk-core "1.11.410"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.410"]
+                 [com.amazonaws/aws-java-sdk-core "1.11.569"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.569"]
                  [democracyworks/squishy "3.0.2"
                     :exclusions [joda-time
                                  org.slf4j/slf4j-simple
                                  org.slf4j/slf4j-api]]
-                 [org.clojure/core.async "0.4.474"]
+                 [org.clojure/core.async "0.4.500"]
                  [democracyworks/utility-fns "0.2.0"]
-                 [net.lingala.zip4j/zip4j "1.3.2"]
+                 [net.lingala.zip4j/zip4j "1.3.3"]
                  [turbovote.resource-config "0.2.1"]
-                 [joplin.jdbc "0.3.10"
+                 [joplin.jdbc "0.3.11"
                   :exclusions [ragtime/ragtime.jdbc]]
                  [ragtime/ragtime.jdbc "0.6.4"]
                  [korma "0.4.3"]
-                 [org.clojure/java.jdbc "0.7.8"]
+                 [org.clojure/java.jdbc "0.7.9"]
                  [org.postgresql/postgresql "42.2.5"
                   :exclusions [org.slf4j/slf4j-simple
                                org.slf4j/slf4j-api]]
-                 [org.xerial/sqlite-jdbc "3.23.1"]
+                 [org.xerial/sqlite-jdbc "3.27.2.1"]
                  [commons-lang/commons-lang "2.6"]
                  [xerces/xercesImpl "2.12.0"]
-                 [com.fasterxml.woodstox/woodstox-core "5.1.0"]
+                 [com.fasterxml.woodstox/woodstox-core "5.2.1"]
                  [me.raynes/fs "1.4.6"]]
   :profiles {:test {:resource-paths ["test-resources"]
                     :dependencies [[com.github.kyleburton/clj-xpath "1.4.11"]]}
