62f1d2f5bf8b3517bb90b1e732df6aad859834da
Liz Starin <liz@democracy.works>
2020-02-12 17:18:07 -0500

    adds trigger function to clear xml table when no feeds are running

---
+++ resources/migrations/20200212-00-clear-xml-table.down.sql
@@ -0,0 +1,2 @@
+DROP FUNCTION clear_xml_table();
+DROP TRIGGER all_feeds_complete ON public.results;
---
+++ resources/migrations/20200212-00-clear-xml-table.up.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION clear_xml_table() RETURNS trigger AS $$
+	BEGIN
+		LOCK TABLE public.results;
+		IF (SELECT COUNT(*) FROM public.results WHERE complete = false) = 0 THEN
+			TRUNCATE TABLE xml_tree_results;
+		END IF;
+		RETURN null;
+	END; $$
+LANGUAGE PLPGSQL;
+
+CREATE TRIGGER all_feeds_complete
+	AFTER UPDATE ON public.results
+	EXECUTE PROCEDURE clear_xml_table();
