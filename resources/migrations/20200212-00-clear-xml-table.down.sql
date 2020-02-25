c7105ca857d0f528eac0091ed0a412bd1568c90e
Liz Starin <liz@democracy.works>
2020-02-25 13:58:57 -0500

    trigger function logs its runs

--- resources/migrations/20200212-00-clear-xml-table.down.sql
+++ resources/migrations/20200212-00-clear-xml-table.down.sql
@@ -1,2 +1,3 @@
 DROP FUNCTION clear_xml_table();
 DROP TRIGGER all_feeds_complete ON public.results;
+DROP TABLE xml_table_truncate_runs;
--- resources/migrations/20200212-00-clear-xml-table.up.sql
+++ resources/migrations/20200212-00-clear-xml-table.up.sql
@@ -1,7 +1,25 @@
-CREATE FUNCTION clear_xml_table() RETURNS trigger AS $$
+CREATE TABLE public.xml_table_truncate_runs (time_of_run TIMESTAMP WITH TIME ZONE,
+    																				 xml_table_size BIGINT,
+    																		 		 xml_table_size_pretty TEXT,
+																						 db_size BIGINT,
+																						 db_size_pretty TEXT);
+
+CREATE OR REPLACE FUNCTION clear_xml_table() RETURNS trigger AS $$
 	BEGIN
-		LOCK TABLE public.results;
+		LOCK TABLE public.results IN EXCLUSIVE MODE;
 		IF (SELECT COUNT(*) FROM public.results WHERE complete = false) = 0 THEN
+			INSERT INTO public.xml_table_truncate_runs(
+				time_of_run,
+				xml_table_size,
+				xml_table_size_pretty,
+				db_size,
+				db_size_pretty)
+				VALUES(
+					(SELECT NOW()),
+					(SELECT PG_TOTAL_RELATION_SIZE('public.xml_tree_values')),
+					(SELECT PG_SIZE_PRETTY(PG_TOTAL_RELATION_SIZE('public.xml_tree_values'))),
+					(SELECT PG_DATABASE_SIZE('dataprocessor')),
+					(SELECT PG_SIZE_PRETTY(PG_DATABASE_SIZE('dataprocessor'))));
 			TRUNCATE TABLE public.xml_tree_values;
 		END IF;
 		RETURN null;
