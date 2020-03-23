dac42f43b02a0c4be79a17d9b36dfd24280b1689
Liz Starin <liz@democracy.works>
2020-03-23 15:02:10 -0400

    xml benchmarking

--- benchmarking.sh
+++ benchmarking.sh
@@ -2,12 +2,12 @@
 
 # files=( "../benchmarking/files/3_xml_medium.zip" "../benchmarking/files/3_xml_big.zip" )
 # for f in "${files[@]}"; do
-for f in ../benchmarking/files/*; do
+for f in ../benchmarking/5.1-files/4-5.1-large-vipFeed-53-2020-02-11.xml; do
   for run in {1..5}; do
-    echo "$f" >> benchmarking-4.txt
-    { time lein run "$f" ; } 2>> benchmarking-4.txt
-    echo "" >> benchmarking-4.txt
-    echo "" >> benchmarking-4.txt
+    echo "$f" >> benchmarking-5.1-large.txt
+    { time lein run "$f" ; } 2>> benchmarking-5.1-large.txt
+    echo "" >> benchmarking-5.1-large.txt
+    echo "" >> benchmarking-5.1-large.txt
 
     psql -U lizstarin -d dataprocessor \
     -c "truncate table public.xml_tree_values;"
