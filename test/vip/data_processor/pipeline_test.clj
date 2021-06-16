68a8e96a8efa01aec98b1739fdce9a1e1a0496e0
Liz Starin <liz@democracy.works>
2021-06-16 14:27:57 -0400

    revises pipeline, adds tests

--- test/vip/data_processor/pipeline_test.clj
+++ test/vip/data_processor/pipeline_test.clj
@@ -21,28 +21,45 @@
 
 (deftest run-pipeline-test
   (testing "Without an exception"
-    (let [ctx {:input 0 :pipeline [incrementor
-                                   incrementor
-                                   incrementor]}
+    (let [counter (atom 0)
+          ctx {:input 0
+               :check-stop-flag-fn #(do (swap! counter inc) identity %)
+               :pipeline [incrementor
+                            incrementor
+                            incrementor]}
           result (run-pipeline ctx)]
       (is (= 3 (:input result)))
+      (is (= 3 @counter))
       (is (empty? (:pipeline result)))
       (is (not (contains? result :stop)))
       (is (not (contains? result :exception)))))
   (testing "With an exception"
-    (let [ctx {:input 0 :pipeline [incrementor
-                                   thrower
-                                   incrementor]}
+    (let [ctx {:input 0
+               :check-stop-flag-fn #(identity %)
+               :pipeline [incrementor
+                            thrower
+                            incrementor]}
           result (run-pipeline ctx)]
       (is (:stop result))
       (is (:exception result))
       (is (= 1 (:input result)))
       (is (= thrower (:thrown-by result)))
-      (is (= [incrementor] (:pipeline result))))))
+      (is (= [incrementor] (:pipeline result)))))
+  (testing "With a stop request"
+    (let [ctx {:input 0
+               :check-stop-flag-fn #(assoc (identity %) :stop "Stop requested by Fakey McName")
+               :pipeline [incrementor
+                            incrementor
+                            incrementor]}
+          result (run-pipeline ctx)]
+      (is (:stop result))
+      (is (= 1 (:input result)))
+      (is (not (empty? (:pipeline result))))
+      (is (not (contains? result :exception))))))
 
 (deftest process-test
   (testing "Builds the context and executes the pipeline"
-    (let [intial-input {:input 0}
+    (let [intial-input {:input 0 :check-stop-flag-fn #(identity %)}
           pipeline [incrementor incrementor incrementor]
           result (process pipeline intial-input)]
       (is (= 3 (:input result)))
