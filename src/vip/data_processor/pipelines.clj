5bdf7e2059b9f4dc337df8f408f54964ed13eec8
Troy Hill <troy@democracy.works>
2021-07-07 08:06:23 -0600

    Clone 5.X XML pipeline to start 6.0 pipeline
    Adds support for processing XML files that have
    the 6.0 spec version. Currently just a clone
    of the 5.X XML pipeline, but can add new 
    validations easily.

--- src/vip/data_processor/pipelines.clj
+++ src/vip/data_processor/pipelines.clj
@@ -2,7 +2,8 @@
   (:require [vip.data-processor.pipelines.csv.v3 :as csv.v3]
             [vip.data-processor.pipelines.csv.v5 :as csv.v5]
             [vip.data-processor.pipelines.xml.v3 :as xml.v3]
-            [vip.data-processor.pipelines.xml.v5 :as xml.v5]))
+            [vip.data-processor.pipelines.xml.v5 :as xml.v5]
+            [vip.data-processor.pipelines.xml.v6 :as xml.v6]))
 
 (defn choose-pipeline [ctx]
   (cond
@@ -24,6 +25,10 @@
     (and (= :xml (:format ctx))
          (= "5.2" (:spec-family ctx)))
     (update ctx :pipeline (partial concat xml.v5/pipeline))
+
+    (and (= :xml (:format ctx))
+         (= "6.0" (:spec-family ctx)))
+    (update ctx :pipeline (partial concat xml.v6/pipeline))
 
     (= :xml (:format ctx))
     (assoc ctx :stop (str "Unsupported XMl version: " (:spec-family ctx)))
