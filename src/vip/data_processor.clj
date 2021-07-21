69a383b9e425a0d76271cbe7cc40e8d02640ca58
Liz Starin <liz@democracy.works>
2021-07-21 09:52:56 -0400

    adds info to stop message

--- src/vip/data_processor.clj
+++ src/vip/data_processor.clj
@@ -35,7 +35,7 @@
      (log/info "New run completed:"
                (psql/get-run result))
      (when stop
-       (q/publish-failure {:stop stop}))
+       (q/publish-failure {:stop stop :start_time (:start_time result)}))
      (if exception
        (q/publish-failure (merge completed-message
                                  {:exception (.getMessage exception)}))
